d6822b1468ed72bd5799cc0c5169c53c62470b44
Jaccojwang <danolago@gmail.com>
2019-02-15 12:48:31 +0300

    Add a function to ensure inputs are numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+
+// function inputCheck(){
+//   if(sideA === NaN || sideB === NaN || sideC === NaN){
+//     alert("Please ensure that all your entries are numbers");
+//   }
+//   else{
+//     alert("done");
+//   }
+// }
+//
+// inputCheck();
+//
+// else if (sideA == null || sideA == " " || sideB == null || sideB == " " || sideC == null || sideC == " ") {
+//   alert("Please ensure none of the fields are left blank");
+// }
+// else{
+
 var sideA = parseInt(prompt("Please enter length of side A"));
 var sideB = parseInt(prompt("Please enter length of side B"));
 var sideC = parseInt(prompt("Please enter length of side C"));
-
 function triangleCheck(){
   if((sideA + sideB) <= sideC || (sideB + sideC) <= sideA || (sideA + sideC) <= sideB){
     alert("This is not a triangle");
