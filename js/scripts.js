769272ee9501dc51171e3b215816f21c0c9a7f38
Jaccojwang <danolago@gmail.com>
2019-02-17 14:42:42 +0300

    Add array containing triangle types and comment code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// //finish this function and add array to hold
 // function inputCheck(){
 //   if(sideA === NaN || sideB === NaN || sideC === NaN){
 //     alert("Please ensure that all your entries are numbers");
@@ -21,22 +21,23 @@ function triangleCheck(){
   var side1 = document.getElementById("sideA").value;
   var side2 = document.getElementById("sideB").value;
   var side3 = document.getElementById("sideC").value;
-
+//Change input to integer value
   var sideA = parseInt(side1);
   var sideB = parseInt(side2);
   var sideC = parseInt(side3);
-
+//Array for triangle types
+  var triangleTypes = ["equilateral", "isosceles", "scalene"];
 //Test triangles for type
   if((sideA + sideB) <= sideC || (sideB + sideC) <= sideA || (sideA + sideC) <= sideB){
-    alert("This is not a triangle");
+    document.getElementById('result').innerHTML = "This is not a triangle";//Display if not triangle
   }
   else if(sideA === sideB && sideB === sideC && sideA === sideC){
-    alert("This is an equilateral trianngle");
+    document.getElementById('result').innerHTML = "This is an " + triangleTypes[0] + " triangle";//Display if equilateral
   }
   else if(sideA === sideB || sideB === sideC || sideA === sideC){
-    alert("This is an isosceles triangle");
+    document.getElementById('result').innerHTML = "This is an " + triangleTypes[1] + " triangle";//Display if isosceles
   }
   else{
-    alert("This is a scalene triangle");
+    document.getElementById('result').innerHTML = "This is a " + triangleTypes[2] + " triangle";//Display if scalene
   }
 }
